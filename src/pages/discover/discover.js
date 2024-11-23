fa66b7f707e0ff574721847b7ee9f73950d5b3bf
Kevin Wang <kenwang3333@gmail.com>
2024-11-23 16:31:33 -0600

    feat: create profiles for every person in discover

--- src/pages/discover/discover.js
+++ src/pages/discover/discover.js
@@ -1,3 +1,21 @@
-export default function Profile() {
-  return <div>Profile</div>;
+import { useEffect, useState } from "react";
+import "./discover.css";
+import OtherUserCard from "../../components/OtherUserCard";
+
+export default function Discover() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    fetch("https://disc-assignment-5-users-api.onrender.com/api/users")
+      .then((res) => res.json())
+      .then((data) => {
+        setUsers(data);
+      });
+  }, []);
+
+  return (
+    <div className="posts">
+      {users && users.map((user) => <OtherUserCard profile={user} />)}
+    </div>
+  );
 }
